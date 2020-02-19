829f3360a5282fa5539a6b2fdf64af14c2dad18b
Edilou <ebernardino@Edilous-MacBook-Pro.local>
2020-02-19 21:02:34 +0800

    Convert customize id sequence SQL to function

--- alter.sql
+++ alter.sql
@@ -1,13 +1,17 @@
-DO
-$$
+DROP FUNCTION IF EXISTS public.f_update_sequence;
+CREATE FUNCTION public.f_update_sequence(_location text)
+  RETURNS void AS
+$func$
 DECLARE
     sequence character varying(255);
     row record;
 BEGIN
-    SELECT current_id_sequence INTO sequence FROM location WHERE name = 'DC0001';
-    FOR row IN SELECT relname FROM pg_class WHERE relkind = 'S'
+    SELECT current_id_sequence INTO sequence FROM location WHERE name = _location;
+    FOR row IN SELECT relname FROM pg_class WHERE relkind = 'S' and relname != 'location_id_seq'
     LOOP
-        EXECUTE 'ALTER SEQUENCE ' || row.relname || ' RESTART ' || sequence || ';';
+        EXECUTE 'ALTER SEQUENCE ' || quote_ident(row.relname) || ' RESTART ' || sequence || ';';
     END LOOP;
-END;
-$$;
+END
+$func$ LANGUAGE plpgsql;
+
+SELECT public.f_update_sequence('DC0001');
